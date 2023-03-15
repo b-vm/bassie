af42721e12d4bc69b0af360c900f9da4a2391c6e
b-vm <bart@vmeps.com>
2023-03-15 16:52:50 +0100

    refactored motor controller code

--- os/motor_controller/motor_controller.ino
+++ os/motor_controller/motor_controller.ino
@@ -9,6 +9,12 @@
 # define N_MOTORS 10
 # define CONTROL_FREQ 2000
 
+# define VERBOSE true
+
+
+// TOOD
+// - ensure micros() overflow is handled correctly
+
 
 class PIDController {
     public:
@@ -49,22 +55,14 @@ unsigned int period = 1000000/CONTROL_FREQ;
 unsigned int targets[N_MOTORS];
 unsigned int encoderValues[N_MOTORS];
 bool commanded = false;
-PIDController pid[N_MOTORS];
+int stepsSinceCommand = 0;
+PIDController pids[N_MOTORS];
 
-
-float parseFloat(String &str) {
-    while (str[0] == ' ') str.remove(0, 1);
-    int i = 0;
-    while (str[i] != ' ') i++;
-    float f = str.substring(0, i).toFloat();
-    str.remove(0, i);
-    return f;
-}
 
 
 void setup() {
     Serial.begin(115200); // for debugging
-    Serial1.begin(115200); // comms with raspberry pi
+    Serial1.begin(115200); // comunication with raspberry pi
 
     assert(MOTOR_PIN_RANGE_END - MOTOR_PIN_RANGE_START + 1 == N_MOTORS);
     assert(ENCODER_PIN_RANGE_END - ENCODER_PIN_RANGE_START + 1 == N_MOTORS);
@@ -79,18 +77,21 @@ void setup() {
     ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear ADC prescaler bits
     ADCSRA |= (1 << ADPS2) | (0 << ADPS1) | (1 << ADPS0); // 32 prescaler = 4 times faster and less accurate
 
-    // wait for raspberry pi to send PID parameters
+    wait_for_handshake();
+}
+
+void wait_for_handshake() {
     while (true) {
         if (Serial1.available() > 0) {
             if (Serial1.read() == 's') {
                 String msg = Serial1.readStringUntil('e');
                 Serial.println("Handshake started. Received: " + msg);
                 for (int i = 0; i < N_MOTORS; i++) {
-                    float kp = parseFloat(msg);
-                    float ki = parseFloat(msg);
-                    float kd = parseFloat(msg);
-                    pid[i] = PIDController(kp, ki, kd, 1.0/CONTROL_FREQ);
-                    Serial.println("PID parameters actuator " + String(i) + ":" + pid[i].getSettings() + msg);
+                    float kp = parseNextFloat(msg);
+                    float ki = parseNextFloat(msg);
+                    float kd = parseNextFloat(msg);
+                    pids[i] = PIDController(kp, ki, kd, 1.0/CONTROL_FREQ);
+                    Serial.println("PID parameters actuator " + String(i) + ":" + pids[i].getSettings() + msg);
                 }
                 Serial.println("Initialization complete");
                 return;
@@ -100,57 +101,75 @@ void setup() {
     }
 }
 
-
-void loop() {
-    if (micros() - timeLastStep < period) {
-        delayMicroseconds(10);
-        return;
-    }
-    timeLastStep = micros();
-    controllerStep();
-    Serial.println("time spent: " + String(micros() - timeLastStep) + "us");
+float parseNextFloat(String &str) {
+    while (str[0] == ' ') str.remove(0, 1);
+    int i = 0;
+    while (str[i] != ' ') i++;
+    float f = str.substring(0, i).toFloat();
+    str.remove(0, i);
+    return f;
 }
 
 
+
+void loop() {
+    wait_if_needed();
+    timeLastStep = micros();
+    controllerStep();
+}
+
+void wait_if_needed() {
+    int waitTime = period - (micros() - timeLastStep);
+    if (waitTime < 0) {
+        delayMicroseconds(waitTime);
+    }
+    if (VERBOSE) {
+        Serial.println("wait time: " + String(waitTime) + "us, available time: " + String(period) + "us");
+    }
+}
+
 void controllerStep() {
-    checkInputs();
-    registerMotorSensors();
+    checkTargetInputs();
+    registerEncoderReadings();
     if (commanded) {
         driveMotors();
     }
+    stepsSinceCommand++;
 }
 
 // TODO: convert to angles?
-void checkInputs() {
-    while (Serial.available() > 0) {
-        char c = Serial.read();
-        if (c == 't') {
+void checkTargetInputs() {
+    while (Serial1.available() > 0) {
+        if (Serial1.read() == 's') {
+            String msg = Serial.readStringUntil('e');
             for (int i = 0; i < N_MOTORS; i++) {
-                targets[i] = Serial.parseInt();
+                targets[i] = parseNextFloat(msg);
             }
-            commanded = true;
         }
-        // TODO deal with e 
+        if (VERBOSE) {
+            Serial.println("steps between commands: " + String(stepsSinceCommand));
+        }
+        commanded = true;
+        stepsSinceCommand = 0;
+        return;
     }
 }
 
 // TODO convert to angles?
-void registerMotorSensors() {
+void registerEncoderReadings() {
+    Serial1.print('d ');
+    Serial1.print(stepsSinceCommand + " ");
     for (int i = 0; i < N_MOTORS; i++) {
         encoderValues[i] = analogRead(i + ENCODER_PIN_RANGE_START);
+        Serial1.print(String(encoderValues[i]) + " ");
     }
-    Serial1.write('s');
-    for (int i = 0; i < N_MOTORS; i++) {
-        Serial1.write(encoderValues[i]);
-        Serial1.write(" ");
-    }
-    Serial1.write('e');
+    Serial1.print('e');
 }
 
 void driveMotors() {
     for (int i = 0; i < N_MOTORS; i++) {
         int error = targets[i] - encoderValues[i];
-        int correction = pid[i].step(error);
+        int correction = pids[i].step(error);
         if (correction > 0) {
             digitalWrite(i + MOTOR_DIRECTION_PIN_RANGE_START, HIGH);
             digitalWrite(i + MOTOR_DIRECTION_PIN_RANGE_START + N_MOTORS, LOW);
@@ -158,9 +177,11 @@ void driveMotors() {
             digitalWrite(i + MOTOR_DIRECTION_PIN_RANGE_START, LOW);
             digitalWrite(i + MOTOR_DIRECTION_PIN_RANGE_START + N_MOTORS, HIGH);
         }
-        analogWrite(i + MOTOR_PIN_RANGE_START, correction);
-
         // TODO clipping
-        Serial.println("actuator " + String(i) + " error: " + String(error) + " correction: " + String(correction));
+        analogWrite(i + MOTOR_PIN_RANGE_START, min(abs(correction), 255));
+
+        if (VERBOSE) {
+            Serial.println("actuator " + String(i) + " error: " + String(error) + " correction: " + String(correction));
+        }
     }
 }
